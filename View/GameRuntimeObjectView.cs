819a37f1ae455f9991452222989578127daa7d9c
Mikhail Kortikov <kortikov.m.r@gmail.com>
2026-03-15 23:42:00 +0300

    GameRuntimeObjectView

---
+++ View/GameRuntimeObjectView.cs
@@ -0,0 +1,56 @@
+using Cysharp.Threading.Tasks;
+using DingoGameObjectsCMS.RuntimeObjects.Objects;
+using DingoUnityExtensions.UnityViewProviders.Core;
+using DingoUnityExtensions.UnityViewProviders.Pools;
+
+namespace DingoGameObjectsCMS.View
+{
+    public abstract class GameRuntimeObjectView : ValueContainer<GameRuntimeObject>
+    {
+        private GameRuntimeObject _previousValue;
+
+        public virtual UniTask SpawnAsync(CollectionViewSpawnOptions spawnOptions) => UniTask.CompletedTask;
+        public virtual UniTask DespawnAsync(CollectionViewSpawnOptions spawnOptions) => UniTask.CompletedTask;
+
+        protected sealed override void PreviousValueFree(GameRuntimeObject previousData)
+        {
+            _previousValue = previousData;
+            OnBeforeGameRuntimeObjectChanged(previousData);
+            base.PreviousValueFree(previousData);
+        }
+
+        protected sealed override void SetValueWithoutNotify(GameRuntimeObject value)
+        {
+            var previousValue = _previousValue;
+            var sameRuntimeObject = IsSameRuntimeObject(previousValue, value);
+
+            if (!sameRuntimeObject && previousValue != null)
+                OnGameRuntimeObjectDestroyed(previousValue);
+
+            SetGameRuntimeObjectWithoutNotify(value);
+
+            if (!sameRuntimeObject && value != null)
+                OnGameRuntimeObjectCreated(value);
+
+            OnGameRuntimeObjectChanged(previousValue, value);
+            _previousValue = null;
+        }
+
+        protected virtual void OnBeforeGameRuntimeObjectChanged(GameRuntimeObject previousValue) { }
+        protected virtual void OnGameRuntimeObjectCreated(GameRuntimeObject value) { }
+        protected virtual void OnGameRuntimeObjectDestroyed(GameRuntimeObject value) { }
+        protected virtual void OnGameRuntimeObjectChanged(GameRuntimeObject previousValue, GameRuntimeObject value) { }
+
+        protected abstract void SetGameRuntimeObjectWithoutNotify(GameRuntimeObject value);
+
+        private static bool IsSameRuntimeObject(GameRuntimeObject a, GameRuntimeObject b)
+        {
+            if (ReferenceEquals(a, b))
+                return true;
+            if (a == null || b == null)
+                return false;
+
+            return a.InstanceId == b.InstanceId && a.StoreId.Equals(b.StoreId);
+        }
+    }
+}
--- View/GameRuntimeObjectsCollection.cs
+++ View/GameRuntimeObjectsCollection.cs
@@ -1,19 +1,21 @@
 using System.Collections.Generic;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using DingoGameObjectsCMS.RuntimeObjects;
 using DingoGameObjectsCMS.RuntimeObjects.Objects;
 using DingoUnityExtensions.Pools.Core;
-using DingoUnityExtensions.UnityViewProviders.Core;
 using DingoUnityExtensions.UnityViewProviders.Pools;
 using UnityEngine;
 
 namespace DingoGameObjectsCMS.View
 {
-    public class GameRuntimeObjectsCollection : ValueContainerDictPoolDepend<IReadOnlyDictionary<long, GameRuntimeObject>, long, GameRuntimeObject, ValueContainer<GameRuntimeObject>>
+    public class GameRuntimeObjectsCollection : AsyncValueContainerDictPoolDepend<IReadOnlyDictionary<long, GameRuntimeObject>, long, GameRuntimeObject, GameRuntimeObjectView>
     {
-        protected override Pool<ValueContainer<GameRuntimeObject>> Factory(ValueContainer<GameRuntimeObject> prefab, GameObject parent) => new(prefab, parent);
+        protected override Pool<GameRuntimeObjectView> Factory(GameRuntimeObjectView prefab, GameObject parent) => new(prefab, parent);
         protected override int GetCount(IReadOnlyDictionary<long, GameRuntimeObject> value) => value.Count;
         protected override GameRuntimeObject GetValue(IReadOnlyDictionary<long, GameRuntimeObject> value, long key) => value[key];
         protected override IOrderedEnumerable<long> GetOrderedKeys(IReadOnlyDictionary<long, GameRuntimeObject> value) => value.Keys.OrderBy(k => k);
+        protected override UniTask OnAfterPullAsync(long key, GameRuntimeObject value, GameRuntimeObjectView valueContainer, CollectionViewSpawnOptions spawnOptions) => valueContainer != null ? valueContainer.SpawnAsync(spawnOptions) : UniTask.CompletedTask;
+        protected override UniTask OnBeforePushAsync(long key, GameRuntimeObject value, GameRuntimeObjectView valueContainer, CollectionViewSpawnOptions spawnOptions) => valueContainer != null ? valueContainer.DespawnAsync(spawnOptions) : UniTask.CompletedTask;
     }
 }
