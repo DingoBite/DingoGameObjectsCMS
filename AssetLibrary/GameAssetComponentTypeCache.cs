fa38d42a461126d0336e00e1e71b3f45d17d8b55
Mikhail Kortikov <kortikov.m.r@gmail.com>
2026-05-10 01:34:21 +0300

    Asset Types Cache

---
+++ AssetLibrary/GameAssetComponentTypeCache.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DingoGameObjectsCMS.AssetObjects;
+
+namespace DingoGameObjectsCMS.AssetLibrary
+{
+    public static class GameAssetComponentTypeCache
+    {
+        private static readonly Dictionary<Type, string> _menuNameByType = new();
+        private static IReadOnlyList<Type> _types;
+
+        public static IReadOnlyList<Type> Types => GetTypes();
+
+        public static IReadOnlyList<Type> GetTypes(bool refresh = false)
+        {
+            if (refresh || _types == null)
+                _types = BuildTypes();
+
+            return _types;
+        }
+
+        public static string GetMenuName(Type type)
+        {
+            if (type == null)
+                return string.Empty;
+
+            if (_types == null)
+                _types = BuildTypes();
+
+            return _menuNameByType.TryGetValue(type, out var menuName) && !string.IsNullOrWhiteSpace(menuName)
+                ? menuName
+                : type.Name;
+        }
+
+        public static void Invalidate()
+        {
+            _types = null;
+            _menuNameByType.Clear();
+        }
+
+        private static IReadOnlyList<Type> BuildTypes()
+        {
+            var result = new List<Type>();
+            _menuNameByType.Clear();
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Type[] types;
+                try
+                {
+                    types = assembly.GetTypes();
+                }
+                catch
+                {
+                    continue;
+                }
+
+                foreach (var type in types)
+                {
+                    if (type == null
+                        || type.IsAbstract
+                        || type.IsInterface
+                        || type == typeof(GameAssetComponent)
+                        || !typeof(GameAssetComponent).IsAssignableFrom(type)
+                        || Attribute.IsDefined(type, typeof(HideInTypeMenuAttribute), inherit: false)
+                        || type.GetConstructor(Type.EmptyTypes) == null)
+                    {
+                        continue;
+                    }
+
+                    _menuNameByType[type] = type.GetCustomAttribute<AddTypeMenuAttribute>()?.MenuName ?? type.Name;
+                    result.Add(type);
+                }
+            }
+
+            return result
+                .OrderBy(GetTypeOrder)
+                .ThenBy(type => _menuNameByType.TryGetValue(type, out var menuName) ? menuName : type.Name)
+                .ToArray();
+        }
+
+        private static int GetTypeOrder(Type type)
+        {
+            return type.GetCustomAttribute<AddTypeMenuAttribute>()?.Order ?? 0;
+        }
+    }
+}
